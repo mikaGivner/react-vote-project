66d8c74176e3752a2166a13ac5efda092572a709
mika <mikagivner123@gmail.com>
2023-02-16 22:31:28 +0200

    purple

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import LandingPage from "./components/landing-page";
 function App() {
   return (
     <div className="App">
-      <h2>hello</h2>
+      <LandingPage></LandingPage>
     </div>
   );
 }
---
+++ src/components/landing-page.js
@@ -0,0 +1,9 @@
+import "./landing-page.css";
+
+export default function LandingPage() {
+  return (
+    <div className="landingPage">
+      <input type="email" placeholder="Email" />;
+    </div>
+  );
+}
